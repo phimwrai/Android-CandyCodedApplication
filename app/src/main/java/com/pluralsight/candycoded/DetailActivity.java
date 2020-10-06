82cd02f88d129454a54f1177901efb88b2a98451
phimwrai <phimwrai@gmail.com>
2020-10-06 20:56:07 +0100

    Share Candy with Intents

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -80,7 +80,7 @@ public class DetailActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     private void createShareIntent() {
-        Intent shareIntent = new Intent();
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED );
